440646059fe5c36d9c954b8d22b8e6ebe87acfff
vpachecoaznar@gmail.com <vpachecoaznar@gmail.com>
2025-09-29 09:02:15 +0200

    serial import for template

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
+import serial
+
 # Open TPS port
 TPS_port = serial.Serial(port="COM7",baudrate=115200,timeout=60)
 if TPS_port.isOpen():
- print("Port is connected")
-# Communication with the instrument
-# Request - send a command to the total station
-# request_id: %RIQ,2107:
-request = "%R1Q,2107:\r\n".encode("ascii")
-TPS_port.write(request)
-# Reply - read response data into the sting-variable out
-response = TPS_port.readline()
-# Extract the returned values
-header, parameters = response.split(b":", 1)
-# Request | Reply | Extract
-# Request | Reply | Extract
-# ...
-# Close TPS port
-TPS_port.close()
+    print("Port is connected")
+    # Communication with the instrument
+    # Request - send a command to the total station
+    # request_id: %RIQ,2107:
+    request = "%R1Q,2107:\r\n".encode("ascii")
+    TPS_port.write(request)
+    # Reply - read response data into the sting-variable out
+    response = TPS_port.readline()
+    # Extract the returned values
+    header, parameters = response.split(b":", 1)
+    # Request | Reply | Extract
+    # Request | Reply | Extract
+    # ...
+    # Close TPS port
+    TPS_port.close()
 if not TPS_port.isOpen():
- print("Port is disconnected")
+    print("Port is disconnected")
